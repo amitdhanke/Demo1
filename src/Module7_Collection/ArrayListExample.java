bb1fc2c1468f371172791591f6d8451cc735b13c
amitdhanke <amit.dhanke8@gmail.com>
2018-06-10 00:45:06 +0530

    comitting arrayListEXample

---
+++ src/Module7_Collection/ArrayListExample.java
@@ -0,0 +1,26 @@
+package Module7_Collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       ArrayList<String> a = new ArrayList<String>();
+       a.add("Amit");
+       a.add("Swaraj");
+       a.add("Amit");
+       a.add(null);
+       
+       Iterator<String> i =a.iterator();
+       
+       while(i.hasNext())
+       {
+    	   System.out.println(i.next());
+    	   
+       }
+       
+	}
+
+}
